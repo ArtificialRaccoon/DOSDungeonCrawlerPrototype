203856572ed6df8824b5ca072239d8779d16b118
Bernard Walker <github@bernardwalker.ca>
2024-12-30 18:44:51 -0500

    Updated Allegro Build
    Fixes issue with the mouse on DOSBOX

--- src/GameProcessor.cpp
+++ src/GameProcessor.cpp
@@ -27,13 +27,6 @@ void GameProcessor::InitializeGame()
 	//Load the default font
 	font = load_bitmap_font(".\\OTHER\\BitScrip.bmp", palette, nullptr);
 
-	//There is a strange bug with my cross compiled version of
-	//DJGPP or Allegro.  If the mouse rubs against the mouse
-	//range for too long, it'll segfault.  This issue happens
-	//even in the example code files, suggesting its the cross
-	//compiled code.  This just solves the crash stupidly.
-	set_mouse_range(SCREEN_WIDTH * -2, SCREEN_HEIGHT * -2, SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2);
-
 	reserve_voices (32, -1);    
     if (install_sound (DIGI_AUTODETECT, MIDI_AUTODETECT, NULL) < 0)
 	{
@@ -50,45 +43,6 @@ void GameProcessor::HandleEvents() { states.back()->ProcessInput(this); }
 void GameProcessor::ProcessEvents() 
 {
 	show_mouse(NULL);
-	poll_mouse();
-
-	//There is some sort of bug in the cross-compiled
-	//version of DJGPP or Allegro which causes a crash if
-	//the mouse hit against the mouse range.  This does
-	//solve the issue (along with making the rage larger
-	//then the screen); it's just dumb.
-	bool mouse_on_screen = true;
-	int newX, newY = 0;
-
-	if (mouse_x < 0) 
-	{
-		newX = 0;
-		mouse_on_screen = false;
-	} 
-	else if (mouse_x > SCREEN_WIDTH - 1) 
-	{
-		newX = SCREEN_WIDTH - 1;
-		mouse_on_screen = false;
-	} 
-	else
-		newX = mouse_x;
-
-	if (mouse_y < 0) 
-	{
-		newY = 0;
-		mouse_on_screen = false;
-	} 
-	else if (mouse_y > SCREEN_HEIGHT - 1) 
-	{
-		newY = SCREEN_HEIGHT - 1;
-		mouse_on_screen = false;
-	} 
-	else
-		newY = mouse_y;
-
-	if (!mouse_on_screen)
-		position_mouse(newX, newY);	
-
 	states.back()->AquireInput(this);
 }
 

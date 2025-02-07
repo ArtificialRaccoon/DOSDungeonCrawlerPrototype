46dadfead468e93d3ee4e507d27aa5f02fb29d65
Bernard Walker <github@bernardwalker.ca>
2025-02-06 20:04:21 -0500

    Minor Commit
    Just adding some additional changes from the rework.

--- TileEd Files/WALLSET.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="WALLSET" tilewidth="8" tileheight="8" tilecount="176" columns="16">
- <image source="Tilesheet.png" trans="000000" width="128" height="88"/>
-</tileset>
